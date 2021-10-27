f731d7b592c8346b8c54b53bf511a464fab52d7e
Chris <chris_thompson@bcit.ca>
2021-10-27 11:03:11 -0700

    Hello world

---
+++ src/HelloWorld.java
@@ -0,0 +1,18 @@
+/**
+ * Hello world!
+ *
+ * @author BCIT
+ * @version 202130
+ */
+public final class HelloWorld {
+
+    private HelloWorld() { }
+
+    /**
+     * Drives the program.
+     * @param args unused
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello world!");
+    }
+}
